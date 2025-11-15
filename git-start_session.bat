719354288fc0f70eca57a4d2ba851d40c3bd8533
petercl8 <lindstrom.peter@gmail.com>
2025-11-15 17:38:25 -0600

    Quick automatic update

--- git-start_session.bat
+++ git-start_session.bat
@@ -1,9 +1,5 @@
 @echo off
 :: ===== Start Session =====
-
-git remote remove origin 2>nul
-git remote add origin git@github.com:petercl8/FlexCNN_for_Medical_Physics.git
-
 :: Fetch latest changes from GitHub
 git fetch origin
 
